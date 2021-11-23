cb2ee041b277cf6486848d52074747d558bfa71b
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-11-23 15:07:48 +0000

    feat(swr): use fallbackData

--- components/AgeGuesserSWR.tsx
+++ components/AgeGuesserSWR.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import useSWR from 'swr';
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 type Props = {
   name: string;
 };
 
 const AgeGuesserSWR = ({ name }: Props) => {
-  const { data, error } = useSWR(`https://api.agify.io/?name=${name}`);
+  const { data, error } = useSWR(
+    `https://api.agify.io/?name=${name}`,
+    fetcher,
+    { fallbackData: { age: '...' } }
+  );
 
   return (
     <div className="p-6 shadow-md rounded-2xl text-center">
       <h2>Fetching with SWR</h2>
-      {error ? (
-        <p>Oops!</p>
-      ) : (
-        <p className="text-8xl">
-          {data?.age === undefined ? '...' : data?.age || '🤷🏻‍♀️'}
-        </p>
-      )}
+      {error ? <p>Oops!</p> : <p className="text-8xl">{data?.age || '🤷🏻‍♀️'}</p>}
     </div>
   );
 };
