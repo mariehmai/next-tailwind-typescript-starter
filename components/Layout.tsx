907ad821c6b97dcc4a3227239aa4446e3b718d35
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-11-23 09:41:34 +0000

    feat(css): add dark mode sample with next-themes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,59 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import { useTheme } from 'next-themes';
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-);
+const Layout = ({ children, title = 'This is the default title' }: Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const switchTheme = () => {
+    if (isMounted) {
+      setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{' '}
+          |{' '}
+          <Link href="/users">
+            <a>Users List</a>
+          </Link>{' '}
+          | <a href="/api/users">Users API</a>
+          <button onClick={switchTheme}>Change theme: {theme}</button>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 import '../styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ThemeProvider attribute="class">
+    <Component {...pageProps} />
+  </ThemeProvider>
 );
 
 export default App;
