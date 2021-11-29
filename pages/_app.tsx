f4cc4f2de24abe5284025f4ba94437d83191bfe4
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-11-29 10:33:25 +0000

    feat(auth): add auth with github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
+
 import { SWRConfig } from 'swr';
 import { QueryClient, QueryClientProvider } from 'react-query';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
 import '../styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <SWRConfig
-    value={{
-      fetcher: (...args) => fetch(args).then((res) => res.json())
-    }}
-  >
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </SWRConfig>
+  <SessionProvider session={pageProps.session}>
+    <SWRConfig
+      value={{
+        fetcher: (...args) => fetch(args).then((res) => res.json())
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </SWRConfig>
+  </SessionProvider>
 );
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,49 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
 import dynamic from 'next/dynamic';
 
 import AgeGuesser from '../components/AgeGuesser';
 
 const Layout = dynamic(() => import('../components/Layout'), { ssr: false });
 
-const IndexPage = () => {
+const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <h1>Loading...</h1>;
+  }
+
+  if (session) {
+    return (
+      <Layout>
+        <div className="p-4">
+          <span>Signed in as {session.user?.name}</span>
+          <button
+            className="ml-2 px-4 py-2 rounded border-gray-800 border-2 hover:shadow-md"
+            type="button"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
+        <AgeGuesser />
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <AgeGuesser />
+      <div className="p-4">
+        <span>Not signed in</span>
+        <button
+          className="ml-2 px-4 py-2 rounded border-gray-800 border-2 hover:shadow-md"
+          type="button"
+          onClick={() => signIn()}
+        >
+          Sign in
+        </button>
+      </div>
     </Layout>
   );
 };
 
-export default IndexPage;
+export default Home;
