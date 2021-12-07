dcd3b6390f17098eaffd817cce0f885afeb98201
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-12-07 16:14:03 +0000

    feat(api): add /api health endpoint

---
+++ pages/api/index.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withSentry } from '@sentry/nextjs';
+
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ statusCode: 200, message: 'OK' });
+};
+
+export default withSentry(handler);
