fd405da8f32b5ed386c287b62932d599c192eda6
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-12-07 16:05:37 +0000

    feat(api): add /users/:id

---
+++ pages/api/users/[id].ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withSentry } from '@sentry/nextjs';
+
+import { sampleUserData } from '../../../utils/sample-data';
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+
+    if (!Array.isArray(sampleUserData)) {
+      throw new Error('Cannot find user data');
+    }
+
+    const userId = id as string;
+
+    if (isNaN(parseInt(userId))) {
+      throw new Error(`Id ${id} is not a valid id.`);
+    }
+
+    const user = sampleUserData.find((user) => user.id == parseInt(userId));
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ statusCode: 400, message: 'User not found' });
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ statusCode: 500, message: err?.message });
+  }
+};
+
+export default withSentry(handler);
