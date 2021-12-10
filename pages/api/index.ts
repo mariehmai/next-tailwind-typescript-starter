4d3d922dce31dc102519f46725af836df9783821
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-12-10 16:19:59 +0000

    feat(sentry): add error back

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { withSentry } from '@sentry/nextjs';
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  throw new Error('API ERROR, not fun!');
   res.status(200).json({ statusCode: 200, message: 'OK' });
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,21 @@ const Layout = dynamic(() => import('../components/Layout'), { ssr: false });
 const Home = () => {
   return (
     <Layout>
+      <button
+        onClick={() => {
+          throw new Error('OMAGAD!!!!!!!!!!!');
+        }}
+      >
+        Click me
+      </button>
       <AgeGuesser />
+      <button
+        onClick={() => {
+          throw new Error('OMAGAD!!!!!!!!!!!');
+        }}
+      >
+        Click me
+      </button>
     </Layout>
   );
 };
