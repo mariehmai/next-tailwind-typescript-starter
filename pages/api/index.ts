c05b4bf03ef55e79fb07c70a340a0937343290df
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-12-10 16:36:18 +0000

    feat(sentry): remove direct error

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { withSentry } from '@sentry/nextjs';
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
-  throw new Error('API ERROR, not fun!');
   res.status(200).json({ statusCode: 200, message: 'OK' });
 };
 
